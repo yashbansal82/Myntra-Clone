0fdc1ac0f754a5f0e2186b6e2589a03c4868bd50
unknown <bansalyash569@gmail.com>
2024-05-04 01:19:57 +0530

    env file

--- 3-myntra-react-clone/src/components/Fetchitems.jsx
+++ 3-myntra-react-clone/src/components/Fetchitems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch(process.env.BACKEND_URI, { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
