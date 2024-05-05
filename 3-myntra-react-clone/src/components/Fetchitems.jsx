79e8203f2ca73498c844338b7b76182da5a2f2c1
yashbansal82 <bansalyash569@gmail.com>
2024-05-06 03:05:36 +0530

    fetchitems changes

--- 3-myntra-react-clone/src/components/Fetchitems.jsx
+++ 3-myntra-react-clone/src/components/Fetchitems.jsx
@@ -14,12 +14,28 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
+    // fetch(import.meta.env.VITE_BACKEND_URI, { signal })
+    //   .then((res) => res.json())
+    //   .then(({ items }) => {
+    //     dispatch(fetchStatusActions.markFetchDone());
+    //     dispatch(fetchStatusActions.markFetchingFinished());
+    //     dispatch(itemsActions.addInitialItems(items[0]));
+    //   });
     fetch(import.meta.env.VITE_BACKEND_URI, { signal })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return res.json();
+      })
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
         dispatch(fetchStatusActions.markFetchingFinished());
         dispatch(itemsActions.addInitialItems(items[0]));
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        // Handle the error appropriately (e.g., display a message to the user)
       });
 
     // return () => {
