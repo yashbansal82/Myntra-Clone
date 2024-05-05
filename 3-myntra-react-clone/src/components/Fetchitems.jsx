00dee4bc9bae2377be7dae93c3dd9630f5a03f03
yashbansal82 <bansalyash569@gmail.com>
2024-05-06 03:28:30 +0530

    chnage

--- 3-myntra-react-clone/src/components/Fetchitems.jsx
+++ 3-myntra-react-clone/src/components/Fetchitems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch(import.meta., { signal })
+    fetch(import.meta.VITE_FRONTEND_URI, { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
